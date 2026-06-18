d0df90f1bcb916f643bce67870f6a35ed77ee2bf
vikramdev772 <vikramdev772@gmail.com>
2026-06-18 10:07:59 +0530

    docker file added

--- backend/src/main/java/jar/BackendApplication.java
+++ backend/src/main/java/jar/BackendApplication.java
@@ -3,11 +3,17 @@ package jar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
+
+        SpringApplication.run(BackendApplication.class, args);
+    }
 
 }
